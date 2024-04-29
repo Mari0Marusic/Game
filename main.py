c29e4dc8d57e25b0b95888f052c8b904e843019e
Mario Marusic <159049503+Mari0Marusic@users.noreply.github.com>
2024-04-29 11:50:47 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, make_response
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    secret_number = request.cookies.get("secret_number")
+
+    response = make_response(render_template("index.html"))
+    if not secret_number:
+        new_secret = random.randint(1, 20)
+        response.set_cookie("secret_number", str(new_secret))
+
+    return response
+
+@app.route("/answer", methods=["POST"])
+def answer():
+    guess = int(request.form.get("guess"))
+    secret_number = int(request.cookies.get("secret_number"))
+
+    if guess == secret_number:
+        message = "Congratulation you guessed a secret number!"
+        response = make_response(render_template("answer.html", message=message))
+        new_secret = random.randint(1, 20)
+        response.set_cookie("secret_number", str(new_secret))
+        return response
+    elif guess > secret_number:
+        message = "Your guess is wrong, Try something smaller!"
+        return render_template("answer.html", message=message)
+    elif guess < secret_number:
+        message = "Your guess is wrong, Try something bigger!"
+        return render_template("answer.html", message=message)
+    
+if __name__ == '__main__':
+    app.run(use_reloader=True)
